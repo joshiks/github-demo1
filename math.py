17990f6d116f818ce317d0d2c577ffed571a0bde
joshiks <56087031+joshiks@users.noreply.github.com>
2021-08-12 12:30:54 +0530

    math.py file uploaded

---
+++ math.py
@@ -0,0 +1,11 @@
+# Add Implementation
+def add(x,y):
+    pass
+# Subtract Implementation
+def subtract(x,y):
+    pass
+# Multiply Implementation
+def multiply(x,y):
+    pass
+def divide(x,y):
+    pass
